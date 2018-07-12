1d3c97d723663a764827b21224b4fea6012b847f
Tyler Smith <tylersmith.me@gmail.com>
2018-07-11 18:09:39 -0700

    REFACTOR: Move errors to bip39.go now that many are removed.

--- bip39.go
+++ bip39.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/binary"
+	"errors"
 	"math/big"
 	"strings"
 
@@ -26,6 +27,23 @@ var (
 
 	// DefaultWordList specifies the wordlist to use upon initialization
 	DefaultWordList = EnglishWordList
+)
+
+var (
+	// ErrInvalidMnemonic is returned when trying to use a malformed mnemonic.
+	ErrInvalidMnemonic = errors.New("Invalid menomic")
+
+	// ErrEntropyLengthInvalid is returned when trying to use an entropy set with
+	// an invalid size.
+	ErrEntropyLengthInvalid = errors.New("Entropy length must be [128, 256] and a multiple of 32")
+
+	// ErrValidatedSeedLengthMismatch is returned when a validated seed is not the
+	// same size as the given seed. This should never happen is present only as a
+	// sanity assertion.
+	ErrValidatedSeedLengthMismatch = errors.New("Seed length does not match validated seed length")
+
+	// ErrChecksumIncorrect is returned when entropy has the incorrect checksum.
+	ErrChecksumIncorrect = errors.New("Checksum incorrect")
 )
 
 func init() {
