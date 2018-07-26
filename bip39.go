f1ae7ff5bfbd68c2a4a20e01ac7be2e1b498ff7d
Tyler Smith <tylersmith.me@gmail.com>
2018-07-25 22:20:51 -0700

    FORMAT: Put exported vars up top.

--- bip39.go
+++ bip39.go
@@ -19,6 +19,23 @@ import (
 )
 
 var (
+	// ErrInvalidMnemonic is returned when trying to use a malformed mnemonic.
+	ErrInvalidMnemonic = errors.New("Invalid menomic")
+
+	// ErrEntropyLengthInvalid is returned when trying to use an entropy set with
+	// an invalid size.
+	ErrEntropyLengthInvalid = errors.New("Entropy length must be [128, 256] and a multiple of 32")
+
+	// ErrValidatedSeedLengthMismatch is returned when a validated seed is not the
+	// same size as the given seed. This should never happen is present only as a
+	// sanity assertion.
+	ErrValidatedSeedLengthMismatch = errors.New("Seed length does not match validated seed length")
+
+	// ErrChecksumIncorrect is returned when entropy has the incorrect checksum.
+	ErrChecksumIncorrect = errors.New("Checksum incorrect")
+)
+
+var (
 	// Some bitwise operands for working with big.Ints
 	last11BitsMask          = big.NewInt(2047)
 	rightShift11BitsDivider = big.NewInt(2048)
@@ -48,23 +65,6 @@ var (
 
 	// wordMap is a reverse lookup map for wordList
 	wordMap map[string]int
-)
-
-var (
-	// ErrInvalidMnemonic is returned when trying to use a malformed mnemonic.
-	ErrInvalidMnemonic = errors.New("Invalid menomic")
-
-	// ErrEntropyLengthInvalid is returned when trying to use an entropy set with
-	// an invalid size.
-	ErrEntropyLengthInvalid = errors.New("Entropy length must be [128, 256] and a multiple of 32")
-
-	// ErrValidatedSeedLengthMismatch is returned when a validated seed is not the
-	// same size as the given seed. This should never happen is present only as a
-	// sanity assertion.
-	ErrValidatedSeedLengthMismatch = errors.New("Seed length does not match validated seed length")
-
-	// ErrChecksumIncorrect is returned when entropy has the incorrect checksum.
-	ErrChecksumIncorrect = errors.New("Checksum incorrect")
 )
 
 func init() {
