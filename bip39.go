3b2d39573b13ef345104a28d60e01c6c1429699c
Paul H. Liu <paul@thev.net>
2021-05-01 10:02:19 -0700

    Typo fix

--- bip39.go
+++ bip39.go
@@ -53,7 +53,7 @@ var (
 
 var (
 	// ErrInvalidMnemonic is returned when trying to use a malformed mnemonic.
-	ErrInvalidMnemonic = errors.New("Invalid mnenonic")
+	ErrInvalidMnemonic = errors.New("Invalid mnemonic")
 
 	// ErrEntropyLengthInvalid is returned when trying to use an entropy set with
 	// an invalid size.
